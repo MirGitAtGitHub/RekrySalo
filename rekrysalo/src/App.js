1896eca3289edb6aa4fe57726486afdb88332c0c
MirGitAtGitHub <89380050+MirGitAtGitHub@users.noreply.github.com>
2022-05-23 09:53:52 +0300

    Poistettu rekrysalo-kansio

--- rekrysalo/src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-import {ToggleButton} from './modules/ToggleButton';
-function App() {
-  return (
-    <ToggleButton />
-  );
-}
-
-export default App;
--- rekrysalo/src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import {Footer} from "../modules/Footer";
-import {TopFooter} from "../modules/TopFooter";
-const Home = () => {
-  return (
-    <TopFooter />,
-      <Footer />
-  )
-}
-export default Home;

d2012a2a4d2851003771b91076b105637019a1cf
Matias Kanerva <matias.kanerva2@gmail.com>
2022-08-16 13:58:30 +0300

    added the third page
    a new live begins :pray:

--- rekrysalo/src/App.js
+++ rekrysalo/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
+import Page3 from "./pages/Page3";
 function App(props) {
   return (
     <BrowserRouter>
       <Routes>
           <Route index element={(props.pagenumber==="1") ? <Page1 /> : "" || (props.pagenumber==="2") ? <Page2 /> : "" || (props.pagenumber===null) ? <Page1 /> : ""} />
+          <Route path={"/Page3"} element={<Page3 />} />
           <Route path={"/Page2"} element={<Page2 />} />
           <Route path={"/Page1"} element={<Page1 />} />
       </Routes>
---
+++ rekrysalo/src/pages/Page3.js
@@ -0,0 +1,66 @@
+import {HomeBackImage} from "../modules/HomeBackImage";
+import {Footer} from "../modules/Footer";
+import{TopFooter} from "../modules/TopFooter";
+import{Link} from "react-router-dom";
+const Page3 = () => {
+  localStorage.setItem("pagenumber","3");
+  return (
+    <html>
+      <body className="Body">
+        <div className="Content">
+            <HomeBackImage />
+
+            <div className="mid">
+                <p className="question">Mieti milloin olet tavoitettavissa:</p>
+                <textarea className="answer" rows="1" cols="70">
+                </textarea>
+
+                <p className="question">Milloin vastaat hakijoille:</p>
+                <textarea className="answer" rows="1" cols="70" style={{"margin-bottom":"50px"}}>
+                </textarea>
+
+                <p className="bluetext">Soittaminen on paras tapa kutsua henkilö haastatteluun!</p>
+
+
+                <p className="question">Luonnostele kutsu haastatteluun:</p>
+                <textarea className="answer" rows="10" cols="70" style={{"margin-bottom":"50px"}}>
+                </textarea>
+
+                <p className="question">Luonnostele vastaus hylätyille hakemuksille:</p>
+                <textarea className="answer" rows="10" cols="70">
+                </textarea>
+                <div>
+                    <input type={"radio"} id="1" name="Valinta" value="A"></input>
+                    <label for="1">Esimerkki A</label>
+                    <input type={"radio"} id="2" name="Valinta" value="B" style={{"margin-bottom":"50px"}}></input>
+                    <label for="2" >Esimerkki B</label>
+                </div>
+
+                <Link to={"/Page" + (parseInt(localStorage.getItem("pagenumber"))-1)}><div class="navigation-button-arrow-left"><h2 class="navigation-button-arrow-text">EDELLINEN</h2></div></Link>
+                <Link to={"/Page" + (parseInt(localStorage.getItem("pagenumber"))+1)}><div class="navigation-button-arrow-right"><h2 class="navigation-button-arrow-text">SEURAAVA</h2></div></Link>
+                
+            </div>
+        </div>
+
+        <div className="Header">
+          <TopFooter text="TYÖPAIKKAILMOITUS" current="2" />
+        </div>
+        
+        <div>
+          {Array(36)
+          .fill()
+          .map((_, i) => (
+            <p key={i}>{i}</p>
+          ))}
+        </div>
+        
+        <div>
+          <Footer />
+        </div>
+      </body>
+
+    </html>
+  )
+}
+
+export default Page3;

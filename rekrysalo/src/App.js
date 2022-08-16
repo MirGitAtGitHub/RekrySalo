fdd4ef891c6b245d4e1e515db4e47e8a5a9b7904
MirGitAtGitHub <89380050+MirGitAtGitHub@users.noreply.github.com>
2022-08-16 13:43:13 +0300

    Muutettu oletusetusivu saven puuttuessa
    Juu.

--- rekrysalo/src/App.js
+++ rekrysalo/src/App.js
@@ -6,7 +6,7 @@ function App(props) {
   return (
     <BrowserRouter>
       <Routes>
-          <Route index element={(props.pagenumber==="1") ? <Page1 /> : "" || (props.pagenumber==="2") ? <Page2 /> : ""} />
+          <Route index element={(props.pagenumber==="1") ? <Page1 /> : "" || (props.pagenumber==="2") ? <Page2 /> : "" || (props.pagenumber===null) ? <Page1 /> : ""} />
           <Route path={"/Page2"} element={<Page2 />} />
           <Route path={"/Page1"} element={<Page1 />} />
       </Routes>

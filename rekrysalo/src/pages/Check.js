c92aecc4be5af717f765ce55c305b8a087182621
MirGitAtGitHub <89380050+MirGitAtGitHub@users.noreply.github.com>
2022-09-30 13:45:38 +0300

    Check and print pages now function
    Cheers to that, non-alcoholic of course.

--- rekrysalo/src/pages/Check.js
+++ rekrysalo/src/pages/Check.js
@@ -22,39 +22,21 @@ const Check = () => {
 
           <p className="question">Mieti, millaista osaamista yrityksesi tarvitsee nyt ja tulevaisuudessa:</p>
           
-          <textarea readOnly className="answer" rows="10" cols="70" id="answer1" style={{"margin-bottom":"40px", "border": "3px solid black", "backgroundColor": "whitesmoke"}} onChange={() => localStorage.setItem("p1a1",document.getElementById("answer1").value)} defaultValue={localStorage.getItem("p1a1")}></textarea>
+          <textarea readOnly className="answer" rows="10" cols="70" id="p1a1" style={{"margin-bottom":"40px", "border": "3px solid black", "backgroundColor": "whitesmoke"}} defaultValue={localStorage.getItem("p1a1")}></textarea>
 
           <p className="question">Pohdi yrityksen lyhyen ja pitkän aikavälin tavoitteet:</p>
-          <textarea readOnly className="answer" rows="10" cols="70" id="answer2" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} onChange={() => localStorage.setItem("p1a2",document.getElementById("answer2").value)} defaultValue={localStorage.getItem("p1a2")}></textarea>
-
-          <div style={{"margin-bottom":"40px", "display": "inline-block"}}>
-            <input disabled type={"radio"} id="r1" name="Valinta" value="A"></input>
-            <label for="r1">Esimerkki A</label>
-            <input disabled type={"radio"} id="r2" name="Valinta" value="B"></input>
-            <label for="r2">Esimerkki B</label>
-            <input disabled type={"radio"} id="r3" name="Valinta" value="C"></input>
-            <label for="r3">Esimerkki C</label>
-          </div>
-
-          
+          <textarea readOnly className="answer" rows="10" cols="70" id="p1a2" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} defaultValue={localStorage.getItem("p1a2")}></textarea>
             
           <p className="question">Aseta rekrytoinnin ajankohta:</p>
           
-          <input readOnly type="datetime-local" id="rekryaika1" name="rekryaika" style={{"margin-right":"20px", "border": "3px solid black", "backgroundColor": "whitesmoke"}} onChange={() => localStorage.setItem("p1d1",document.getElementById("rekryaika1").value)} defaultValue={localStorage.getItem("p1d1")}></input>
-          <input readOnly type="datetime-local" id="rekryaika2" name="to_rekryaika" style={{"margin-right":"20px", "border": "3px solid black", "backgroundColor": "whitesmoke"}} onChange={() => localStorage.setItem("p1d2",document.getElementById("rekryaika2").value)} defaultValue={localStorage.getItem("p1d2")}></input>
+          <input readOnly type="datetime-local" id="rekryaika1" name="rekryaika" style={{"margin-right":"20px", "border": "3px solid black", "backgroundColor": "whitesmoke"}} defaultValue={localStorage.getItem("p1d1")}></input>
+          <input readOnly type="datetime-local" id="rekryaika2" name="to_rekryaika" style={{"margin-right":"20px", "border": "3px solid black", "backgroundColor": "whitesmoke"}} defaultValue={localStorage.getItem("p1d2")}></input>
 
           
           <p className="question" style={{"margin-top":"40px"}}>Arvioi budjetti rekrytoinnille:</p>
           
           
-          <input readOnly className="number-input" type="text" id="budjetti" name="budjetti" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} defaultValue={localStorage.getItem("p1n1")} onBlur={(e) => {var a = [];
-    var k = e.which;
-    
-    for (var i = 48; i < 58; i++)
-        a.push(i);
-    
-    if (!(a.indexOf(k)>=0))
-        e.preventDefault();if (parseInt(document.getElementById("budjetti").value) > 20000) {document.getElementById("budjetti").value = 20000;alert("Maksimibudjetti on 20 000 euroa.");localStorage.setItem("p1n1",document.getElementById("budjetti").value);} if (parseInt(document.getElementById("budjetti").value) < 2000) {document.getElementById("budjetti").value = 2000;alert("Minimibudjetti on 2000 euroa.");localStorage.setItem("p1n1",document.getElementById("budjetti").value)};if (parseInt(document.getElementById("budjetti").value) >= 2000 && parseInt(document.getElementById("budjetti").value) <= 20000) {document.getElementById("budjetti").value = document.getElementById("budjetti").value;localStorage.setItem("p1n1",document.getElementById("budjetti").value)}}}></input>
+          <input readOnly className="number-input" type="text" id="budjetti" name="budjetti" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} defaultValue={localStorage.getItem("p1n1")}></input>
           <p className="question" style={{"display": "inline-block"}}>€</p>
           <div></div>
 
@@ -63,101 +45,98 @@ const Check = () => {
           {/*Page2*/}
 
           <p className="question" style={{"margin-top":"60px"}}>Yrityksen sijainti:</p>
-            <textarea readOnly className="answer" rows="1" cols="70" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} id="answer1" onChange={() => localStorage.setItem("p2a1",document.getElementById("answer1").value)} defaultValue={localStorage.getItem("p2a1")}></textarea>
+            <textarea readOnly className="answer" rows="1" cols="70" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} id="p2a1" defaultValue={localStorage.getItem("p2a1")}></textarea>
 
             <div style={{"margin-bottom":"40px"}}>
-              <input type={"radio"} id="1" name="Valinta" value="E"></input>
+              <input type={"checkbox"} id="1" name="Valinta" value="1" defaultChecked={(localStorage.getItem("p2cb1") === "true") ? true : false} disabled></input>
               <label for="1">Etätyö tai monta sijaintia</label>
-              <input type={"radio"} id="2" name="Valinta" value="F"></input>
+              <input type={"checkbox"} id="2" name="Valinta" value="2" defaultChecked={(localStorage.getItem("p2cb2") === "true") ? true : false} disabled></input>
               <label for="2">Työpaikalla on eri sijainti kuin yrityksellä</label>
             </div>
+
+            <div id="etatiedottaisijainnit" style={{display:(localStorage.getItem("p2cb1") === "true") ? "" : "none"}}>
+              <p className="question" id="tpsijaintip">Etätyön tiedot tai työpaikan sijainnit:</p>
+              <textarea readOnly className="answer" rows="1" cols="70" id="optionalanswer1" defaultValue={localStorage.getItem("p2oa1")}></textarea>
+            </div>
+
+            <div id="tyopaikansijainti" style={{display:(localStorage.getItem("p2cb2") === "true") ? "" : "none"}}>
+              <p className="question" id="tpsijaintip">Työpaikan sijainti:</p>
+              <textarea readOnly className="answer" rows="1" cols="70" id="optionalanswer2" defaultValue={localStorage.getItem("p2oa2")}></textarea>
+            </div>
+
 
 
             <p className="question">Miten kuvailisit yrityksen toimintaa?</p>
 
-            <textarea readOnly className="answer" rows="10" cols="70" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"40px"}} id="answer2" onChange={() => localStorage.setItem("p2a2",document.getElementById("answer2").value)} defaultValue={localStorage.getItem("p2a2")}></textarea>
+            <textarea readOnly className="answer" rows="10" cols="70" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"40px"}} id="p2a2" defaultValue={localStorage.getItem("p2a2")}></textarea>
 
             <p className="question">Työsopimuksen tyyppi:</p>
-            <select className="Dropdown" name="valinta" id="Työ1" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"10px"}} onChange={() => localStorage.setItem("p2s1",document.getElementById("Työ1").value)} defaultValue={localStorage.getItem("p2s1")}>
+            <select className="Dropdown" name="valinta" id="P2Työ1" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"10px"}} defaultValue={localStorage.getItem("p2s1")}>
               <option disabled className="boxed">Määräaikainen</option>
               <option disabled className="boxed">Toistaiseksi voimassa oleva</option>
               <option disabled className="boxed">Harjoittelija</option>
             </select>
 
             <p className="question">Työaika:</p>
-            <select className="Dropdown" name="valinta" id="Työ2" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"10px"}} onChange={() => localStorage.setItem("p2s2",document.getElementById("Työ2").value)} defaultValue={localStorage.getItem("p2s2")}>
+            <select className="Dropdown" name="valinta" id="P2Työ2" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"10px"}} defaultValue={localStorage.getItem("p2s2")}>
               <option disabled className="boxed">Kokoaikainen</option>
               <option disabled className="boxed">Osa-aikainen</option>
             </select>
             
             <p className="question">Tehtävänimike:</p>
-            <textarea readOnly className="answer" rows="1" cols="70" id="answer3" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"40px"}} onChange={() => localStorage.setItem("p2a3",document.getElementById("answer3").value)} defaultValue={localStorage.getItem("p2a3")}></textarea>
+            <textarea readOnly className="answer" rows="1" cols="70" id="p2a3" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"40px"}} defaultValue={localStorage.getItem("p2a3")}></textarea>
 
             
             <p className="question">Työn kuvaus:</p>
-            <textarea readOnly className="answer" rows="10" cols="70" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"50px"}} id="answer4" onChange={() => localStorage.setItem("p2a4",document.getElementById("answer4").value)} defaultValue={localStorage.getItem("p2a4")}></textarea>
+            <textarea readOnly className="answer" rows="10" cols="70" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"50px"}} id="p2a4" defaultValue={localStorage.getItem("p2a4")}></textarea>
 
             <p className="question">Ilmoittajan nimi:</p>
-            <textarea readOnly className="answer" rows="1" cols="70" id="answer5" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} onChange={() => localStorage.setItem("p2a5",document.getElementById("answer5").value)} defaultValue={localStorage.getItem("p2a5")}></textarea>
+            <textarea readOnly className="answer" rows="1" cols="70" id="p2a5" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} defaultValue={localStorage.getItem("p2a5")}></textarea>
 
             <p className="question">Ilmoittajan puhelin:</p>
-            <textarea readOnly className="answer" rows="1" cols="70" id="answer6" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} onChange={() => localStorage.setItem("p2a6",document.getElementById("answer6").value)} defaultValue={localStorage.getItem("p2a6")}></textarea>
+            <textarea readOnly className="answer" rows="1" cols="70" id="p2a6" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} defaultValue={localStorage.getItem("p2a6")}></textarea>
 
             <p className="question">Ilmoita sähköpostiosoite johon hakemukset vastaanotetaan:</p>
 
-            <textarea readOnly className="answer" rows="1" cols="70" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"20px"}} id="answer7" onChange={() => localStorage.setItem("p2a7",document.getElementById("answer7").value)} defaultValue={localStorage.getItem("p2a7")}></textarea>
+            <textarea readOnly className="answer" rows="1" cols="70" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"20px"}} id="p2a7" defaultValue={localStorage.getItem("p2a7")}></textarea>
 
 
             <div style={{"margin-bottom":"40px"}}>
               <label className="question">Ilmoituksen julkaisuaika:</label><br></br>
 
-              <input readOnly type="datetime-local" id="julkaisuaika1" name="julkaisuaika" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-right":"20px"}} onChange={() => localStorage.setItem("p2d1",document.getElementById("julkaisuaika1").value)} defaultValue={localStorage.getItem("p2d1")}></input>
-              <input readOnly type="datetime-local" id="julkaisuaika2" name="to_julkaisuaika" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}}onChange={() => localStorage.setItem("p2d2",document.getElementById("julkaisuaika2").value)} defaultValue={localStorage.getItem("p2d2")}></input>
+              <input readOnly type="datetime-local" id="julkaisuaika1" name="julkaisuaika" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-right":"20px"}} defaultValue={localStorage.getItem("p2d1")}></input>
+              <input readOnly type="datetime-local" id="julkaisuaika2" name="to_julkaisuaika" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} defaultValue={localStorage.getItem("p2d2")}></input>
             </div>
 
             <p className="question">Yhteyshenkilöt ja heidän yhteystietonsa:</p>
-            <textarea readOnly className="answer" rows="1" cols="70" id="answer8" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"50px"}} onChange={() => localStorage.setItem("p2a8",document.getElementById("answer8").value)} defaultValue={localStorage.getItem("p2a8")}> </textarea>
+            <textarea readOnly className="answer" rows="1" cols="70" id="p2a8" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"50px"}} defaultValue={localStorage.getItem("p2a8")}> </textarea>
           
           
 
             {/*Page 3*/}
 
             <p className="question" style={{"margin-top": "60px"}}>Miten ja milloin olet tavoitettavissa hakemuksiin liittyen?</p>
-            <textarea readOnly className="answer" rows="1" cols="70" id="answer1" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"10px"}} onChange={() => localStorage.setItem("p3a1",document.getElementById("answer1").value)} defaultValue={localStorage.getItem("p3a1")}></textarea>
+            <textarea readOnly className="answer" rows="1" cols="70" id="p3a1" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"10px"}} defaultValue={localStorage.getItem("p3a1")}></textarea>
 
             <p className="question">Aseta vastausaika hakemuksiin liittyviin kysymyksiin:</p>
-            <textarea readOnly className="answer" rows="1" cols="70" id="answer2" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"50px"}} onChange={() => localStorage.setItem("p3a2",document.getElementById("answer2").value)} defaultValue={localStorage.getItem("p3a2")}></textarea>
+            <textarea readOnly className="answer" rows="1" cols="70" id="p3a2" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"50px"}} defaultValue={localStorage.getItem("p3a2")}></textarea>
 
             <p className="question">Luonnostele kutsu haastatteluun:</p>
-            <textarea readOnly className="answer" rows="10" cols="70" id="answer3" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"50px"}} onChange={() => localStorage.setItem("p3a3",document.getElementById("answer3").value)} defaultValue={localStorage.getItem("p3a3")}></textarea>
+            <textarea readOnly className="answer" rows="10" cols="70" id="p3a3" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"50px"}} defaultValue={localStorage.getItem("p3a3")}></textarea>
 
             <p className="question">Miten hylättyihin hakemuksiin vastataan? Luo vastauspohja.</p>
-            <textarea readOnly className="answer" rows="10" cols="70" id="answer4" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} onChange={() => localStorage.setItem("p3a4",document.getElementById("answer4").value)} defaultValue={localStorage.getItem("p3a4")}></textarea>
-
-            <div>
-              <input disabled type={"radio"} id="1" name="Valinta" value="A"></input>
-              <label for="1">Esimerkki A</label>
-              <input disabled type={"radio"} id="2" name="Valinta" value="B" style={{"margin-bottom":"50px"}}></input>
-              <label for="2" >Esimerkki B</label>
-            </div>
-
-
+            <textarea readOnly className="answer" rows="10" cols="70" id="p3a4" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} defaultValue={localStorage.getItem("p3a4")}></textarea>
 
             {/*Page 4*/}
 
             <p className="question" style={{"margin-top": "60px"}}>Miten työhön haastattelu toteutetaan?</p>
-            <select className="Dropdown" name="valinta" id="Työ1" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"40px"}} onChange={() => localStorage.setItem("p4s1",document.getElementById("Työ1").value)} defaultValue={localStorage.getItem("p4s1")}>
+            <select className="Dropdown" name="valinta" id="P4Työ1" style={{"border": "3px solid black", "backgroundColor": "whitesmoke", "margin-bottom":"40px"}} defaultValue={localStorage.getItem("p4s1")}>
               <option disabled className="boxed">Ryhmähaastattelu</option>
               <option disabled className="boxed">Etähaastattelu</option>
             </select>
 
             <p className="question">Suunnittele haastattelurunko:</p>
-            <textarea readOnly className="answer" rows="10" cols="70" id="answer1" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} onChange={() => localStorage.setItem("p4a1",document.getElementById("answer1").value)} defaultValue={localStorage.getItem("p4a1")}></textarea>
-
-            <div>
-              <input disabled type={"checkbox"} id="1" name="Esimerkki" value="Esimerkki" style={{"margin-bottom":"50px"}}></input>
-              <label for="1">Esimerkkikysymyksiä:</label>
-            </div>
+            <textarea readOnly className="answer" rows="10" cols="70" id="p4a1" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} defaultValue={localStorage.getItem("p4a1")}></textarea>
 
         </div>
         <div className="empty-div"></div>
--- rekrysalo/src/pages/Print.js
+++ rekrysalo/src/pages/Print.js
@@ -7,7 +7,7 @@ const Print = () => {
   localStorage.setItem("pagenumber","8");
   return (
   <html style={{"background-image": "none"}}>
-    <div>
+    <div style={{paddingLeft: "5%"}}>
       <div >
         <div >
           
@@ -15,10 +15,10 @@ const Print = () => {
           <h1>Sivu 1</h1>
           <h3 >Mieti, millaista osaamista yrityksesi tarvitsee nyt ja tulevaisuudessa:</h3>
           
-          <p  id="answer1" style={{color: "black"}}>{localStorage.getItem("p1a1")}</p>
+          <p  id="p1a1" style={{color: "black"}}>{localStorage.getItem("p1a1")}</p>
 
           <h3>Pohdi yrityksen lyhyen ja pitkän aikavälin tavoitteet:</h3>
-          <p id="answer2" style={{color: "black"}}>{localStorage.getItem("p1a2")}</p>
+          <p id="p1a2" style={{color: "black"}}>{localStorage.getItem("p1a2")}</p>
 
           
             
@@ -38,20 +38,29 @@ const Print = () => {
 
           {/*Page2*/}
           <h1>Sivu 2</h1>
-          <p className="question" style={{"margin-top":"60px"}}>Yrityksen sijainti:</p>
-            <textarea readOnly className="answer" rows="1" cols="70" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}} id="answer1" onChange={() => localStorage.setItem("p2a1",document.getElementById("answer1").value)} defaultValue={localStorage.getItem("p2a1")}></textarea>
+          <h3>Yrityksen sijainti:</h3>
+            <p style={{color: "black"}} id="p2a1">{localStorage.getItem("p2a1")}</p>
 
             <div style={{"margin-bottom":"40px"}}>
-              <input type={"radio"} id="1" name="Valinta" value="E"></input>
+              <input type={"checkbox"} id="1" name="Valinta" value="1" defaultChecked={(localStorage.getItem("p2cb1") === "true") ? true : false} disabled></input>
               <label for="1">Etätyö tai monta sijaintia</label>
-              <input type={"radio"} id="2" name="Valinta" value="F"></input>
+              <input type={"checkbox"} id="2" name="Valinta" value="2" defaultChecked={(localStorage.getItem("p2cb2") === "true") ? true : false} disabled></input>
               <label for="2">Työpaikalla on eri sijainti kuin yrityksellä</label>
             </div>
 
+            <div id="etatiedottaisijainnit" style={{display:(localStorage.getItem("p2cb1") === "true") ? "" : "none"}}>
+              <h3>Etätyön tiedot tai työpaikan sijainnit:</h3>
+              <p id="optionalanswer1" style={{color: "black"}}>{localStorage.getItem("p2oa1")}</p>
+            </div>
+
+            <div id="tyopaikansijainti" style={{display:(localStorage.getItem("p2cb2") === "true") ? "" : "none"}}>
+              <h3>Työpaikan sijainti:</h3>
+              <p id="optionalanswer2" style={{color: "black"}}>{localStorage.getItem("p2oa2")}</p>
+            </div>
 
             <h3>Miten kuvailisit yrityksen toimintaa?</h3>
 
-            <p style={{color: "black"}} id="answer2">{localStorage.getItem("p2a2")}</p>
+            <p style={{color: "black"}} id="p2a2">{localStorage.getItem("p2a2")}</p>
 
             <h3>Työsopimuksen tyyppi:</h3>
             <p id="Työ1" style={{color: "black"}}>{localStorage.getItem("p2s1")}</p>
@@ -60,21 +69,21 @@ const Print = () => {
             <p id="Työ2" style={{color: "black"}}>{localStorage.getItem("p2s2")}</p>
             
             <h3>Tehtävänimike:</h3>
-            <p id="answer3" style={{color: "black"}}>{localStorage.getItem("p2a3")}</p>
+            <p id="p2a3" style={{color: "black"}}>{localStorage.getItem("p2a3")}</p>
 
             
             <h3>Työn kuvaus:</h3>
-            <p style={{color: "black"}} id="answer4">{localStorage.getItem("p2a4")}</p>
+            <p style={{color: "black"}} id="p2a4">{localStorage.getItem("p2a4")}</p>
 
             <h3>Ilmoittajan nimi:</h3>
-            <p id="answer5" style={{color: "black"}}>{localStorage.getItem("p2a5")}</p>
+            <p id="p2a5" style={{color: "black"}}>{localStorage.getItem("p2a5")}</p>
 
             <h3>Ilmoittajan puhelin:</h3>
-            <p id="answer6" style={{color: "black"}}>{localStorage.getItem("p2a6")}</p>
+            <p id="p2a6" style={{color: "black"}}>{localStorage.getItem("p2a6")}</p>
 
             <h3>Ilmoita sähköpostiosoite johon hakemukset vastaanotetaan:</h3>
 
-            <p style={{color: "black"}} id="answer7">{localStorage.getItem("p2a7")}</p>
+            <p style={{color: "black"}} id="p2a7">{localStorage.getItem("p2a7")}</p>
 
 
 
@@ -84,23 +93,23 @@ const Print = () => {
               <input readOnly type="datetime-local" id="julkaisuaika2" name="to_julkaisuaika" style={{"border": "3px solid black", "backgroundColor": "whitesmoke"}}onChange={() => localStorage.setItem("p2d2",document.getElementById("julkaisuaika2").value)} defaultValue={localStorage.getItem("p2d2")}></input>
 
             <h3>Yhteyshenkilöt ja heidän yhteystietonsa:</h3>
-            <p id="answer8" style={{color: "black"}}>{localStorage.getItem("p2a8")}</p>
+            <p id="p2a8" style={{color: "black"}}>{localStorage.getItem("p2a8")}</p>
           
           
 
             {/*Page 3*/}
             <h1>Sivu 3</h1>
             <h3>Miten ja milloin olet tavoitettavissa hakemuksiin liittyen?</h3>
-            <p id="answer1" style={{color: "black"}}>{localStorage.getItem("p3a1")}</p>
+            <p id="p3a1" style={{color: "black"}}>{localStorage.getItem("p3a1")}</p>
 
             <h3>Aseta vastausaika hakemuksiin liittyviin kysymyksiin:</h3>
-            <p id="answer2" style={{color: "black"}}>{localStorage.getItem("p3a2")}</p>
+            <p id="p3a2" style={{color: "black"}}>{localStorage.getItem("p3a2")}</p>
 
             <h3>Luonnostele kutsu haastatteluun:</h3>
-            <p id="answer3" style={{color: "black"}}>{localStorage.getItem("p3a3")}</p>
+            <p id="p3a3" style={{color: "black"}}>{localStorage.getItem("p3a3")}</p>
 
             <h3>Miten hylättyihin hakemuksiin vastataan? Luo vastauspohja.</h3>
-            <p id="answer4" style={{color: "black"}}>{localStorage.getItem("p3a4")}</p>
+            <p id="p3a4" style={{color: "black"}}>{localStorage.getItem("p3a4")}</p>
 
 
 
@@ -109,15 +118,10 @@ const Print = () => {
             <h1>Sivu 4</h1>
 
             <h3>Miten työhön haastattelu toteutetaan?</h3>
-            <p style={{color: "black"}} id="Työ1">{localStorage.getItem("p4s1")}</p>
+            <p style={{color: "black"}} id="P4Työ1">{localStorage.getItem("p4s1")}</p>
 
             <h3>Suunnittele haastattelurunko:</h3>
-            <p id="answer1" style={{color: "black"}}>{localStorage.getItem("p4a1")}</p>
-
-            <div>
-              <input disabled type={"checkbox"} id="1" name="Esimerkki" value="Esimerkki" style={{"margin-bottom":"50px"}}></input>
-              <label for="1">Esimerkkikysymyksiä:</label>
-            </div>
+            <p id="p4a1" style={{color: "black"}}>{localStorage.getItem("p4a1")}</p>
 
         </div>
       </div>
